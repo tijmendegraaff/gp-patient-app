43fddd11ebce2b15192eaa9558e44704b1d32594
Tijmen de Graaff <tijmen76@gmail.com>
2018-08-18 02:03:34 +0200

    setup login functions

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity,
+} from 'react-native';
 
 class SignInScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
 
   render() {
+    const { email, password } = this.state;
     return (
       <View>
-        <Text>SignInScreen</Text>
+        <Text>Login Screen</Text>
+        <Text>email</Text>
+        <TextInput
+          value={email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(input) => {
+            this.setState({ email: input });
+          }}
+        />
+        <Text>password</Text>
+        <TextInput
+          value={password}
+          autoCapitalize="none"
+          secureTextEntry
+          onChangeText={(input) => {
+            this.setState({ password: input });
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            console.log(this.state);
+          }}
+        >
+          <View>
+            <Text>Send</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.navigate('Register');
+          }}
+        >
+          <View>
+            <Text>Register</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
